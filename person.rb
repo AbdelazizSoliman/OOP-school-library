92f16a74306e1be37a004cb2dbe8fb8ae021ae9e
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-08-03 17:50:03 +0300

    Create the has-many side of Person and Rental

--- person.rb
+++ person.rb
@@ -1,29 +1,43 @@
 require_relative 'nameable'
-require_relative 'capitalize_decoder'
-require_relative 'trimmer_deocrator'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
 
-class Person
-  attr_accessor :name, :age
-  attr_reader :id
-
-  def initialize(age, name: 'unknown', parent_permission: true)
-    @id = id
-    @name = name
-    @age = age
-    @parent_permission = parent_permission
-  end
-
-  def correct_name
-    @name
-  end
-
-  def can_use_services?
-    of_age? || @parent_permission
-  end
-
-  private
-
-  def of_age?
-    @age >= 18
-  end
+class Person < Nameable
+    attr_accessor :name, :age
+    attr_reader :id, :rentals
+  
+    def initialize(age, name = 'unknown', parent_permission: true)
+      super()
+      @id = Random.rand(1..188)
+      @name = name
+      @age = age
+      @parent_permission = parent_permission
+      @rentals = []
+    end
+  
+    def add_rental(rental)
+      @rentals << rental
+      rental.person = self
+    end
+  
+    def can_use_services?
+      of_age? || @parent_permission
+    end
+  
+    def correct_name
+      @name
+    end
+  
+    private
+  
+    def of_age?
+      @age >= 18
+    end
 end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name 
