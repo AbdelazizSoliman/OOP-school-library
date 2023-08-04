4e3521e0779e14a3ff39748c7ca94ea867e665f3
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-08-04 17:37:39 +0300

    Create independent method classroom

--- student.rb
+++ student.rb
@@ -1,16 +1,14 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
 
-  def initialize(classroom, age, name = 'unknown', parent_permission: true)
+  def initialize(_classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
-    @classroom = classroom
-    add_to_classroom
   end
 
-  def add_to_classroom
-    @classroom.add_student(self) unless @classroom.students.include?(self)
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
