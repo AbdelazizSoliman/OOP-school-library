9e4f7078ae05257c433ec957493d70e20832989d
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-08-05 00:47:23 +0300

    Create a person

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require_relative 'book'
 require_relative 'person'
+require_relative 'teacher'
+require_relative 'student'
 
 class App
     def initialize
@@ -28,5 +30,17 @@ class App
         puts 'All people:'
         @people.each do |person|
           puts "[#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+        end
+      end
+
+      def create_person
+        puts 'If you want to create a Student please press(1) or press (2) if you want to create Teacher:'
+        choice = gets.chomp.to_i
+        person_creator = PersonCreator.new(@people)
+        case choice
+        when 1
+          person_creator.create_student
+        when 2
+          person_creator.create_teacher
         end
       end
